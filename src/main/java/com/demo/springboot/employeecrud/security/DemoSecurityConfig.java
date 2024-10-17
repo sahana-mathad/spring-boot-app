a8a7218307d10dbab985dfad43f3036092534500
sahana10 <sahana1098@gmail.com>
2024-10-17 16:20:56 +0530

    Added JDBC Spring Security with custom tables

--- src/main/java/com/demo/springboot/employeecrud/security/DemoSecurityConfig.java
+++ src/main/java/com/demo/springboot/employeecrud/security/DemoSecurityConfig.java
@@ -9,35 +9,57 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    //Add Support for JDBC
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}john123")
-                .roles("EMPLOYEE")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}mary123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails sahana = User.builder()
-                .username("sahana")
-                .password("{noop}sahana123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,mary,sahana);
-
-
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        //define the query to fetch users by username from custom table -members
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id,pw,active from members where user_id=?"
+        );
+        //define the query to fetch roles by username from custom table - roles
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id,role from roles where user_id=?"
+        );
+        return jdbcUserDetailsManager;
     }
+
+    // The below code is for InMemory User Details Settings
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}john123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}mary123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails sahana = User.builder()
+//                .username("sahana")
+//                .password("{noop}sahana123")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,mary,sahana);
+//
+//
+//    }
 
     //restricting access based on roles
     @Bean
