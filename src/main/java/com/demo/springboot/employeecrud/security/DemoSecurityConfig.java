c6caf731fce06d4fdbdf89aaf7962a2a2f1096e0
sahana10 <sahana1098@gmail.com>
2024-10-17 22:44:20 +0530

    Disabled Spring Security

--- src/main/java/com/demo/springboot/employeecrud/security/DemoSecurityConfig.java
+++ src/main/java/com/demo/springboot/employeecrud/security/DemoSecurityConfig.java
@@ -1,87 +1,87 @@
-package com.demo.springboot.employeecrud.security;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DemoSecurityConfig {
-
-    //Add Support for JDBC
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        //define the query to fetch users by username from custom table -members
-        jdbcUserDetailsManager.setUsersByUsernameQuery(
-                "select user_id,pw,active from members where user_id=?"
-        );
-        //define the query to fetch roles by username from custom table - roles
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-                "select user_id,role from roles where user_id=?"
-        );
-        return jdbcUserDetailsManager;
-    }
-
-    // The below code is for InMemory User Details Settings
-
+//package com.demo.springboot.employeecrud.security;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DemoSecurityConfig {
+//
+//    //Add Support for JDBC
 //    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails john = User.builder()
-//                .username("john")
-//                .password("{noop}john123")
-//                .roles("EMPLOYEE")
-//                .build();
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
 //
-//        UserDetails mary = User.builder()
-//                .username("mary")
-//                .password("{noop}mary123")
-//                .roles("EMPLOYEE","MANAGER")
-//                .build();
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        //define the query to fetch users by username from custom table -members
+//        jdbcUserDetailsManager.setUsersByUsernameQuery(
+//                "select user_id,pw,active from members where user_id=?"
+//        );
+//        //define the query to fetch roles by username from custom table - roles
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+//                "select user_id,role from roles where user_id=?"
+//        );
+//        return jdbcUserDetailsManager;
+//    }
 //
-//        UserDetails sahana = User.builder()
-//                .username("sahana")
-//                .password("{noop}sahana123")
-//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-//                .build();
+//    // The below code is for InMemory User Details Settings
 //
-//        return new InMemoryUserDetailsManager(john,mary,sahana);
+////    @Bean
+////    public InMemoryUserDetailsManager userDetailsManager() {
+////        UserDetails john = User.builder()
+////                .username("john")
+////                .password("{noop}john123")
+////                .roles("EMPLOYEE")
+////                .build();
+////
+////        UserDetails mary = User.builder()
+////                .username("mary")
+////                .password("{noop}mary123")
+////                .roles("EMPLOYEE","MANAGER")
+////                .build();
+////
+////        UserDetails sahana = User.builder()
+////                .username("sahana")
+////                .password("{noop}sahana123")
+////                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(john,mary,sahana);
+////
+////
+////    }
 //
+//    //restricting access based on roles
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+//                .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+//                .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN"));
+//
+//        //use HTTP basic auth
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        //disable csrf
+//        http.csrf(
+//                csrf-> csrf.disable()
+//        );
+//
+//        return http.build();
 //
 //    }
-
-    //restricting access based on roles
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN"));
-
-        //use HTTP basic auth
-        http.httpBasic(Customizer.withDefaults());
-
-        //disable csrf
-        http.csrf(
-                csrf-> csrf.disable()
-        );
-
-        return http.build();
-
-    }
-}
+//}
